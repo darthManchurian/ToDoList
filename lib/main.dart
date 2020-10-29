5f65a94d1057d12bb87f4fd043e4c6bbc0ec80e4
rumaan-2512 <syedrpasha@gmail.com>
2020-10-29 17:15:01 +0530

    working app ready!

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list/database/todolist_item.dart';
 import 'database/db.dart';
 
 void main() async {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Color.fromRGBO(100,100,100, 1.0),
         accentColor: Color.fromRGBO(255, 150, 66,1.0)
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'TODOLIST'),
     );
   }
 }
@@ -33,37 +34,151 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<ToDoItem> _todolist= [];
+  String _name;
 
-  void _incrementCounter() {
+  var text;
+  List<Widget> get _item => _todolist.map((item) => format(item)).toList();
+
+  Widget format(ToDoItem item)
+  {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+      child: Dismissible(
+        key: Key(item.id.toString()),
+        child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Theme.of(context).accentColor,
+            shape: BoxShape.rectangle,
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Colors.black,
+                blurRadius: 5,
+                offset: Offset(0.0,5)
+              )
+            ]
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: Text(item.name,
+                  style: TextStyle(color: Colors.black,
+                      fontSize: 16),
+                ),
+              ),
+
+            ],
+
+
+
+          )
+        ),
+        onDismissed: (DismissDirection d){
+          DB.delete(ToDoItem.table, item);
+          refresh();
+        },
+
+      ),
+
+    );
+  }
+
+  void _create(BuildContext context)
+  {
+    showDialog(
+        context: context,
+      builder: (BuildContext context){
+          return AlertDialog(
+            backgroundColor: Theme.of(context).accentColor,
+            title: Text('ADD TASK',
+              style: TextStyle(color: Theme.of(context).primaryColor,),
+            ),
+            content: Form(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextField(
+                    autofocus: true,
+                    decoration: InputDecoration(
+                      labelText: 'item name'
+                    ),
+                    onChanged: (name){
+                      _name = name;
+                    },
+                  )
+                ],
+              ),
+            ),
+            actions: [
+              FlatButton(
+                color: Theme.of(context).accentColor,
+                onPressed: () => _save(),
+                child: Text('save',
+                style: TextStyle(color: Theme.of(context).primaryColor,),
+              )
+              )
+            ],
+          );
+      }
+
+    );
+
+  }
+
+  void _save() async{
+    Navigator.of(context).pop();
+    ToDoItem item = ToDoItem(
+      name: _name
+    );
+    await DB.insert(ToDoItem.table, item);
     setState(() {
-      _counter++;
     });
+    refresh();
+
+}
+
+  void initState()
+  {
+    refresh();
+    super.initState();
+  }
+
+  void refresh() async{
+    List<Map<String,dynamic>> _results = await DB.query(ToDoItem.table);
+    _todolist = _results.map((item) => ToDoItem.fromMap(item)).toList();
+    setState(() => _name = '');
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Container(
+        child: ListView(
+          children: <Widget> [
+            Padding(
+              padding: EdgeInsets.fromLTRB(20, 20, 0, 10),
+              child: Text("Keep track of stuff.",
+                style: TextStyle(color: Theme.of(context).accentColor,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ) ,),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            ListView(
+              children: _item,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+            )
+
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () => _create(context),
+        tooltip: 'Add new task',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
